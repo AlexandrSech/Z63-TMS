c566d7b68eb9d5ecfcdcc174f0ef4d5ceb0a516f
Sergey <ZerGey1994MU@mail.ru>
2021-11-21 09:16:45 +0300

    Homework #2 added

---
+++ students/shloma/002_homework_02/task_2_1.py
@@ -0,0 +1,10 @@
+# Task 2.1
+
+# Read string from console
+s = input()
+
+# New string
+s3 = s[2]
+
+print("Input string:", s)
+print("Third symbol:", s3)
---
+++ students/shloma/002_homework_02/task_2_3.py
@@ -0,0 +1,10 @@
+# Task 2.3
+
+# Read string from console
+s = input()
+
+# New string
+s_new = s[:5]
+
+print("Input string:", s)
+print("Firs 5 symbols:", s_new)
