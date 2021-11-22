5eef2a77495de3b0b3e6a7cb6b25902a1b50b2e5
Sergey Shloma <ZerGey1994MU@mail.ru>
2021-11-22 19:19:39 +0300

    Added task_6_3

--- students/shloma/006_homework_06/task_6.py
+++ students/shloma/006_homework_06/task_6.py
@@ -34,3 +34,14 @@ for i in range(n):
 print(f"\n2) Максимальный элемент матрицы: {max_mtx}")
 # or
 # print(f"Максимальный элемент матрицы: {max([max(matrix[i]) for i in range(n)])}")
+
+# 6.3) Search MIN element:
+min_mtx = min(matrix[0])
+
+for i in range(n):
+    if min(matrix[i]) < max_mtx:
+        min_mtx = min(matrix[i])
+
+print(f"3) Минимальный элемент матрицы: {min_mtx}")
+# or
+# print(f"Минимальный элемент матрицы: {min([min(matrix[i]) for i in range(n)])}")
