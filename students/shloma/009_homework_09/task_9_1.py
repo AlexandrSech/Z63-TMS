e1311d6527ec99c9d817c3c4ad9dc111d2a4dfff
Sergey Shloma <ZerGey1994MU@mail.ru>
2021-11-26 21:23:32 +0300

    Added homework #9 task_9_1

---
+++ students/shloma/009_homework_09/task_9_1.py
@@ -0,0 +1,12 @@
+# Task 9.1
+# shlom41k
+
+my_strs = ["Hello, shlom41k",
+           "This ia string",
+           "and one more string",
+           "this is a 4'th string",
+           "33333333333333",
+           "London is the capital of Great Britan"
+           ]
+
+[print(f"{ind} - {s}")for ind, s in enumerate(my_strs, 1)]
