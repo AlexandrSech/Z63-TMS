d5820b6d8a635ab11a0dc9e92f77e1560bdb48b3
Данил Дюжов <dyuzhov17@gmail.com>
2021-11-28 17:25:22 +0300

    task_1_5

---
+++ students/Dyuzhov_D_G/task_1_5.py
@@ -0,0 +1,7 @@
+import math
+a = 4
+b = 12
+hypotenuse = math.sqrt((a ** 2) + (b ** 2))
+area = 0.5 * (a * b)
+print('Гипатенуза прямоугольного треугольника = ', hypotenuse)
+print('Площадь прямоугольного треугольника = ', area)
