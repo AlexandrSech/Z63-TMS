34d6f41145925defca877f1326e042b56c33eece
Данил Дюжов <dyuzhov17@gmail.com>
2021-11-29 20:35:26 +0300

    task_1_3_4_var2

--- students/Dyuzhov_D_G/Homework/01_homework/task_1_4.py
+++ students/Dyuzhov_D_G/Homework/01_homework/task_1_4.py
@@ -1,4 +1,3 @@
-'''
 import math
 a = 2
 b = 6
@@ -6,17 +5,18 @@ n = math.sqrt(a * b)
 с = (a + b)/2
 print('Средне геометрическое значение чисел = ', n)
 print('Средне арефметическое значение чисел = ', с)
-'''
 
+
+exit()
 
 import math
 def the_numbers_g (a, b):
     p = math.sqrt(a * b)
-    return p
-def the_numbers_n (a, b):
-    c = (a + b)/2
-    return c
-geometry = the_numbers_g(3, 4)
-arephmetics = the_numbers_n(3, 4)
+    c = (a + b) / 2
+    return p, c
+
+geometry, arephmetics = the_numbers_g(4, 6)
+
 print('Средне геометрическое значение чисел = ', geometry)
 print('Средне арефметическое значение чисел = ', arephmetics)
+
