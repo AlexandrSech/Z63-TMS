e50106405e0a43543206557a8fe5665ff97a305e
Данил Дюжов <dyuzhov17@gmail.com>
2021-11-29 20:39:29 +0300

    task_3_2

--- students/Dyuzhov_D_G/Homework/03_homework/task_3_1.py
+++
@@ -1,8 +0,0 @@
-for guests in range(1, 60):
-    print('Если колличество гостей составит', str(guests))
-    if guests >=50:
-        print('Заказываем ресторан')
-    elif guests >=20 and guests <= 50:
-        print('Заказываем кафе')
-    elif guests < 20:
-        print('Празднуем дома')
