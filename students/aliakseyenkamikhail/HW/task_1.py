f0a70886527e6a65355cda754dab810f0a47c8c0
mikhailaliakseyenka <aliakseyenkamikhail@gmail.com>
2021-12-03 22:23:16 +0300

    +HW

---
+++ students/aliakseyenkamikhail/HW/task_1.py
@@ -0,0 +1,12 @@
+# 1. Даны 2 действительных числа a и b. Получить их сумму, разность и произведение.
+
+a = float(input('enter float numbes '))
+b = float(input('enter float numbes '))
+
+sum_=a+b
+raz=a-b
+pr=a*b
+print(sum_, raz, pr)
+
+#. Даны действительные числа x и y. Получить |x| -|y| / 1+|y|
+
---
+++ students/aliakseyenkamikhail/HW/task_4_3.py
@@ -0,0 +1,12 @@
+# 3) Дан словарь: {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+# Добавить каждому ключу число равное длине этого ключа 
+# (пример {‘key’: ‘value’} -> {‘key3’:‘value’}). Чтобы получить список
+#  ключей - использовать метод .keys()
+
+my_dict = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+
+for items in my_dict:
+    
+    print(items)
+
+print (my_dict)
