712f05ba4234eef5c3eac97fc2df1be8025bce83
Artsem Vorobiev <adbwork@mail.ru>
2021-12-03 20:07:24 +0300

    task_4.1.py

---
+++ students/vorobievpapka/HomeWork/task_4.1.py
@@ -0,0 +1,7 @@
+first_list = [1, 2, 3, 4, 5, 6, 7, 8]
+second_list = []
+for i in first_list :
+    new_znach = i * 2
+    second_list.append(new_znach)
+print("first list =",first_list)
+print("second list =",second_list)
