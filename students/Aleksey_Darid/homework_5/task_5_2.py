2e44df4174370abcdc5e05ffae593b7340996795
aleksey <alekseydarid@gmail.comm>
2021-12-05 15:08:09 +0300

    kd

---
+++ students/Aleksey_Darid/homework_5/task_5_2.py
@@ -0,0 +1,17 @@
+def d(x):
+    i = 1
+    f = 0
+    while i <= x:
+        f += i
+        i += 1
+    return f
+def r(x):
+    i = 1
+    f = 1
+    while i <= x:
+        f *= i
+        i += 1
+    return f
+
+print(d(6))
+print(r(6))
