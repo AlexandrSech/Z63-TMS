451845ac20bfd58bb30534a5ee8a4b48f1e5d3b7
shlom41k <ZerGey1994MU@mail.ru>
2021-12-08 09:24:32 +0300

    Added requirements.txt

--- students/shloma/014_homework_14/task_14_2/argparser.py
+++ students/shloma/014_homework_14/task_14_2/argparser.py
@@ -14,8 +14,8 @@ def pomodoro_argparse():
     parser.add_argument("-fn", "--first-name", dest="first_name", type=str, help="First name", default="Ivan")
     parser.add_argument("-ln", "--last-name", dest="last_name", type=str, help="Last name", default="Ivanov")
     parser.add_argument("-f", "--focusing", dest="focus", type=int, help="Focusing time (minutes)", default=25)
-    parser.add_argument("-p", "--pause", dest="pause", type=int, help="Breaking time (minutes)", default=4)
-    parser.add_argument("-c", "--cycles", dest="cycle", type=int, help="Number of cycles", default=5)
+    parser.add_argument("-p", "--pause", dest="pause", type=int, help="Breaking time (minutes)", default=5)
+    parser.add_argument("-c", "--cycles", dest="cycle", type=int, help="Number of cycles", default=4)
     parser.add_argument("-t", "--task", dest="task", type=str, help="Tank name", default="Programming")
 
     args = parser.parse_args()
