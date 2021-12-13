119d7bccd30a252c0e137ff4de9cded9b6feff4f
Artsem Vorobiev <adbwork@mail.ru>
2021-12-13 13:40:58 +0300

    ispravil

--- students/vorobievpapka/HomeWork/task_4_1.py
+++ students/vorobievpapka/HomeWork/task_4_1.py
@@ -5,21 +5,4 @@
 """
 
 first_list = [1, 2, 3, 4, 5, 6, 7, 8]
-second_list = []
-for i in first_list :
-    new_znach = i * -2
-    second_list.append(new_znach)
-
-print("first list =",first_list)
-print("second list =",second_list)
-
-
-second_list_wh = []
-z = 0
-while z < len(first_list):
-    new_znach2 = first_list[0] * -2
-    second_list_wh.append(new_znach2)
-    z += 1
-    first_list[0] += 1 # как лучше записать?
-
-print("second list =",second_list_wh)
+print([num * -2 for num in first_list])
--- students/vorobievpapka/HomeWork/task_4_3.py
+++ students/vorobievpapka/HomeWork/task_4_3.py
@@ -4,7 +4,7 @@
 ‘value’}). Чтобы получить список ключей - использовать метод .keys()
 (подсказка: создается новый ключ с цифрой в конце, старый удаляется)
 """
-
+# {(k + len(k)): v for k,v in test_dict.items()}
 
 dict = {
     'test': 'test_value',
@@ -19,13 +19,25 @@ test_dict = {
     'age': '20',
 
 }
-dict1 = dict.copy()
-print(dict)
-print(dict1)
-keys = dict1.keys()
-print(keys)
-dict1.update(test_dict)
-print(dict1)
-
-for i in keys:
-    3
+# dict1 = dict.copy()
+# print(dict)
+# print(dict1)
+# keys = dict1.keys()
+# print(keys)
+# dict1.update(test_dict)
+# print(dict1)
+#
+# for i in keys:
+#     3
+# b = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+# print({key + str(len(key)): value for key, value in b.items()})
+# for k, v in b.items():
+#     print('key', k)
+#     print('value', v)
+#
+# print(list(b.keys()))
+# print(b.values())
+# print(b.items())
+# a = 1, 2
+a, b = (2, 3)
+print(a, b)
