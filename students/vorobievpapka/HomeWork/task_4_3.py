4c83ff85b90662de2586b5668ab284b6403e5deb
Artsem Vorobiev <adbwork@mail.ru>
2021-12-14 19:26:28 +0300

    task_4_1

--- students/vorobievpapka/HomeWork/task_4_3.py
+++ students/vorobievpapka/HomeWork/task_4_3.py
@@ -6,13 +6,19 @@
 """
 # {(k + len(k)): v for k,v in test_dict.items()}
 
-dict = {
+dict_ = {
     'test': 'test_value',
     'europe': 'eur',
     'dollar': 'usd',
     'ruble': 'rub'
 
 }
+result_dict = dict()
 
-print({key + str(len(key)): value for key, value in dict.items()})
+# print({key + str(len(key)): value for key, value in dict.items()})
+
+for key in dict_.keys():
+    result_dict[key + str(len(key))] = dict_[key]
+
+print(result_dict)
 
