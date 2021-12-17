7440ddc18379e16c171878d6335e16f2e95296d2
Artsem Vorobiev <adbwork@mail.ru>
2021-12-17 21:47:32 +0300

    class_work_17_12

---
+++ students/vorobievpapka/class_work/chat.py
@@ -0,0 +1,12 @@
+from classes import User, Server
+
+server = Server()
+
+users_list = []
+for i in range(5):
+    users_list.append(User(str(i), server))
+
+for user in users_list:
+    user.send_message("afdfdsdsdd " + user.nick_name)
+    user.get_message_list(is_print=True)
+    print()
