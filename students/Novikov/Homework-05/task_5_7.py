ba944edc9bc39b2397d9fc4cec9905301b91cd78
AntonNov <anton.nov123@gmail.com>
2021-12-24 15:41:24 +0300

    made some tasks

--- students/Novikov/Homework-05/task_5_7.py
+++ students/Novikov/Homework-05/task_5_7.py
@@ -1,8 +1,20 @@
 """7) Дана целочисленная квадратная матрица. Найти в каждой строке наибольший элемент
 и поменять его местами с элементом главной диагонали. """
 
-matrix = [ [1, 2, 3],
-           [6, 5, 4],
-           [9, 8, 7] ]
+import random
 
-print(my_list)
+n = 5
+matrix = list()
+for _ in range(n):
+    matrix.append([random.randint(-10, 10) for _ in range(n)])
+
+print("Исходная матрица: ")
+for line in matrix:
+    print(*line, sep="\t\t")
+
+for i in range(0, len(matrix)):
+    matrix[i][i] = max(matrix[i])
+
+print("Полученная матрица: ")
+for line in matrix:
+    print(*line, sep="\t\t")
