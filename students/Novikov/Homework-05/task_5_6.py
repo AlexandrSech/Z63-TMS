4129279a785adc48c23a1ed4e8d710387f359b74
AntonNov <anton.nov123@gmail.com>
2021-12-24 16:29:45 +0300

    6th task

--- students/Novikov/Homework-05/task_5_6.py
+++ students/Novikov/Homework-05/task_5_6.py
@@ -3,14 +3,16 @@
 
 import random
 
-my_list = list()
-for _ in range(100):
-    my_list.append(random.randint(-100, 100))
-print(my_list)
-
-count_of_intervals = 0
-temp = my_list[0]
-for _ in range(1, len(list))
-
-
-
+lst = [random.randint(0, 20) for el in range(40)]
+print(lst)
+result = 0
+count = 0
+for j in range(len(lst) - 2):
+    if lst[j + 2] > lst[j + 1] > lst[j]:
+        count += 1
+    elif count >= 1 and lst[j + 1] > lst[j + 2]:
+        result += 1
+        count = 0
+if lst[-1] > lst[-2] > lst[-3]:
+    result += 1
+print(result)
