9b92fa7b89fcfb80646b94a529cee1580e8eafa9
shlom41k <ZerGey1994MU@mail.ru>
2021-12-28 11:42:55 +0300

    Modified some HW

--- students/shloma/008_homework_08/task_8_2.py
+++ students/shloma/008_homework_08/task_8_2.py
@@ -1,6 +1,12 @@
 # Task 8.2
 # shlom41k
 
+"""
+Даны слова. Выяснить, является ли хоть одно из них палиндромом. 
+(Определить функцию, позволяющую распознавать слова палиндромы.)
+"""
+
+
 # Проверяем слово
 def is_word_polyndrom(word):
     return True if word.lower() == word.lower()[::-1] else False
@@ -12,9 +18,11 @@ def is_str_polyndrom(s):
     return True if s.lower() == s.lower()[::-1] else False
 
 
-# Test
-words = ["lol", "топот", "lolli", "powop", "student"]
-s = "А роза упала на лапу Азора"
+if __name__ == '__main__':
 
-[print(f"'{word}' {'is poly' if is_word_polyndrom(word) else 'is not poly'}") for word in words]
-print(f"\n'{s}' {'is poly string' if is_str_polyndrom(s) else 'is not poly string'}")
+    # Test
+    words = ["lol", "топот", "lolli", "powop", "student"]
+    s = "А роза упала на лапу Азора"
+
+    [print(f"{i}) '{word}' {'is poly' if is_word_polyndrom(word) else 'is not poly'}") for i, word in enumerate(words, 1)]
+    print(f"\n-> '{s}' {'is poly string' if is_str_polyndrom(s) else 'is not poly string'}")
