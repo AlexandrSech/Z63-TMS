f62ddc980011d52dd24e3df3555316e27fd92853
AntonNov <anton.nov123@gmail.com>
2022-01-02 02:24:11 +0300

    task from 4th homework

--- students/Novikov/Homework-04/task_4_4.py
+++ students/Novikov/Homework-04/task_4_4.py
@@ -3,5 +3,7 @@
 my_list = list(range(1, 6))
 print("Было: ", my_list)
 
-my_list.append(my_list.pop(0))
-print("Стало: ", my_list)
+my_list2 = list(my_list)
+
+my_list2.append(my_list2.pop(0))
+print("Стало: ", my_list2)
