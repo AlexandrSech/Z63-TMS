4932a4bb967dcf62a2d92abc95d4da862ade0edc
AntonNov <anton.nov123@gmail.com>
2022-01-03 18:04:27 +0300

    reformat task_9.2.py

--- students/Novikov/Homework-09/task_9_2.py
+++ students/Novikov/Homework-09/task_9_2.py
@@ -1,6 +1,4 @@
 """2. Создать lambda функцию, которая принимает на вход неопределенное
 количество именных аргументов и выводит словарь с ключами удвоенной длины. {‘abc’: 5} -> {‘abcabc’: 5}"""
 
-f = lambda **kwargs: {key * 2: value for key, value in kwargs.items()}
-
-print(f(abc=5, one="Texas"))
+print((lambda **kwargs: {key * 2: value for key, value in kwargs.items()})(abc=5, one="Texas"))
