56f44f7842537f5993de3e1a2df386175e4eb355
aleksey <alekseydarid@gmail.comm>
2022-01-08 12:22:44 +0300

    hw10

--- students/Aleksey_Darid/homework_10/task_10_1.py
+++ students/Aleksey_Darid/homework_10/task_10_1.py
@@ -10,6 +10,6 @@ people = [
     ["Sexy", "Mariya", 18]
 ]
 
-with open("/homework_10/filename", "w", newline="") as file:
+with open(filename, "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(people)
